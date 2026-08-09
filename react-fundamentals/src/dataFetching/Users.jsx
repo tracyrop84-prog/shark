b99d306ade9a8a1c1d1b9b94260f63f581788a9f
Tracy Kiprop <tracyrop84@gmail.com>
2026-08-09 14:33:55 +0300

    completed fetch and displaying data using axios

--- react-fundamentals/src/dataFetching/Users.jsx
+++ react-fundamentals/src/dataFetching/Users.jsx
@@ -4,30 +4,47 @@ import { useEffect, useState } from "react";
 function Users() {
   //create a state for users
   const [users, setUsers] = useState([]);
+  //create loading state
+  const [loading, setLoading] = useState(true);
+  //error state
+  const [error, setError] = useState("");
   //Fetching data
+
   useEffect(() => {
-    const getUsers=async()=>{
+    const getUsers = async () => {
+      try {
+        const response = await axios.get("https://randomuser.me/api/");
+        setUsers(response.data.results);
+        setLoading(false); //turn off loading after request
+      } catch (err) {
+        console.log(err);
+        setError("failed to load users")
+        setLoading(false); //if it fail stops loading
+      }
+    };
 
-      try{
-const response= await axios.get("https://randomuser.me/api/")
-setUsers(response.data.results)
-      }catch(error){console.log(error)}
-
-    }
-    
-    getUsers()
-    
+    getUsers();
   }, []);
   return (
     <>
       <h1>Users</h1>
-      {users.map((user)=>{
-        return(<div key={user.login.uuid}>
-         <p>{user.name.first} {user.name.last}</p>
-        <p>{user.gender}</p>
-         <p>{user.location.street.number} {user.location.street.name}</p>
-        </div>)
-      })}
+      {loading ? (
+        <p>Loading users...</p>
+      ) :error ?(<p>{error}</p>): (
+        users.map((user) => {
+          return (
+            <div key={user.login.uuid}>
+              <p>
+                {user.name.first} {user.name.last}
+              </p>
+              <p>{user.gender}</p>
+              <p>
+                {user.location.street.number} {user.location.street.name}
+              </p>
+            </div>
+          );
+        })
+      )}
     </>
   );
 }
